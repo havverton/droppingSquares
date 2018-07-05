aecd32e4cab1660c4bf46caa3a07dd02da2442e1
Havverton <havverton@gmail.com>
2018-07-06 02:18:42 +0300

    Наконец-то квадратов несколько и они падают

--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@ var cvs = document.getElementById("canvas");
 var ctx = cvs.getContext("2d");
 
 var fps=1000/60;
-var squares = [];
-
+var squares = [50];
+var lastX=0;
+var lastY=0;
 
 
 function Square(x,y){
@@ -11,97 +12,94 @@ function Square(x,y){
     this.height = 50;
     this.color = "#000";
     this.speed = 10; //Math.floor(Math.random()*5+1)+1;
-    this.x = x;
-    this.y = y;
+    this.x;
+    this.y;
 
-
-this.fall = function (){
-                  
-                    
-                    ctx.clearRect(0,0,600,600);
-                    //отрисовка квадрата
-                    ctx.fillStyle = this.color;
-                    ctx.fillRect(this.x,this.y,this.height, this.width);
-                   // alert("Это х " + this.x + " Это Y " + this.y )
-                   this.y+=this.speed;
-
-}
-
-   
-}
-  
-/*function drop(square){
-    var timerId = setTimeout(function tick() {
-        square.fall();
-        timerId = setTimeout(tick, 1000);
-      }, 1000);
-  }
-*/
-
-
-  function game(){
-    for(var i = 0; i<=100; i++){
-    var square = new Square(getRandX(),0);
-    squares.push(square); }
-           squares.forEach(function(square){
-                square.fall();
-              //  debugger;
-           
-        })
-   
+    if(x<=lastX||x>=lastX+50){
+        lastX=x;
+        this.x=x;
+    }else{
+        x+=50;
+        this.x=x;
     }
-  
 
-  
+    if(y<=lastY||y>=lastY+50){
+        lastY=y;
+        this.y=y;
+    }else{
+        y-=50;
+        this.y=y;
+    }
+
+//падение квадрата
+this.fall = function (){
+        this.y+=this.speed;
+        if(this.y>=550){
+            this.y=y;
+        }
+    }
+//отрисовка
+this.draw = function() {
+    ctx.fillRect(this.x,this.y,this.height, this.width);
+    
+}
+//смена цвета
+this.changeColor = function(color) {
+    ctx.fillStyle = color;
+}
+   
+}
+
+
+  //очистка экрана 60 кад/cек
+function clearScreen(){
+    setInterval(function(){
+        ctx.clearRect(0,0,600,600);
+    },fps)
+}
+
+function game(){
+
+    for(var i = 0; i< 10; i++){
+    squares[i] = new Square(getRandX(),getRandY());
+  //  debugger;
+    }
+     clearScreen(); 
+     game2();
+    }
+ function game2(){
+    setInterval(function(){
+    for(var i = 0; i < 10; i++){
+        squares[i].fall();
+        squares[i].draw();
+       //alert(squares[i].y);
+        
+
+ }
+},fps)
+ }
+
+
+function show(){
+    for(var i = 0; i< squares.length; i++){
+        squares[i].fall;
+        squares[i].draw;
+        }
+}
+
 game();
-
- 
-  
-
-
 
 function getRandX(){
     var x=Math.floor(Math.random()*550);
     return x;
 }
 
-function getRandOffsetY(){
-    var x=Math.floor(Math.random()*(10-5))+5;
+function getRandY(){
+    var x=Math.floor((Math.random()*-500+(-100))-100);
     return x;
 }
 
-
-/*
-function startGame(){
-    setInterval(function (){
-       
-       
-        var i=square.y;
-        var n=square.x;
-        var d=square.delay;
-                //Интервал падения
-                var timerId = setTimeout(function () 
-                {
-                   
-                      //очистка квадрата
-                  // ctx.fillStyle = "#fff";
-                  ctx.clearRect(0,0,600,600)
-
-                    //отрисовка квадрата
-                    ctx.fillStyle = square.color;
-                    ctx.fillRect(n,i,square.height, square.width);
-                    
-                 ;
-
-
-                    i+=square.offsetY;
-
-                }
-                    ,square.delay);
-                //Интервал падения
-        
-                square.x = getRandX();
-                square.offsetY = getRandOffsetY();}
-                
-        , 1000);
+function getRandOffsetY(){
+    var x=Math.floor(Math.random()*(10-5))+5;
+    return x;
 }

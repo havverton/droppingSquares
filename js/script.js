565f9c887f057785ffc9156184355afc6a2a8438
Havverton <havverton@gmail.com>
2018-07-05 17:04:09 +0300

    как сделать многа кавадратаф

--- js/script.js
+++ js/script.js
@@ -6,54 +6,51 @@ var squares = [];
 
 
 
-function Square(x){
-this.width = 50;
-this.height = 50;
-this.color = "#000";
-this.speed = 1; //Math.floor(Math.random()*5+1)+1;
-this.x = x;
-this.y = 10;
+function Square(x,y){
+    this.width = 50;
+    this.height = 50;
+    this.color = "#000";
+    this.speed = 10; //Math.floor(Math.random()*5+1)+1;
+    this.x = x;
+    this.y = y;
 
 
 this.fall = function (){
                   
-                    this.y+= this.speed;
-                    ctx.clearRect(0,0,600,600)
+                    
+                    ctx.clearRect(0,0,600,600);
                     //отрисовка квадрата
                     ctx.fillStyle = this.color;
                     ctx.fillRect(this.x,this.y,this.height, this.width);
                    // alert("Это х " + this.x + " Это Y " + this.y )
-                   
-     
+                   this.y+=this.speed;
 
 }
 
-
-
- 
+   
 }
-
-
-
   
-function drop(square){
+/*function drop(square){
     var timerId = setTimeout(function tick() {
         square.fall();
         timerId = setTimeout(tick, 1000);
       }, 1000);
   }
-  
+*/
+
+
   function game(){
     for(var i = 0; i<=100; i++){
-    var square = new Square(getRandX()); 
-    squares.push(square);
-    
-        squares.forEach(function(square){
-            drop(square);
+    var square = new Square(getRandX(),0);
+    squares.push(square); }
+           squares.forEach(function(square){
+                square.fall();
+              //  debugger;
+           
         })
    
     }
-  }
+  
 
   
 game();
@@ -108,82 +105,3 @@ function startGame(){
                 
         , 1000);
 }
-
-/*
-function startGame(){
-    setInterval(function (){
-       
-       
-        var i=square.y;
-        var n=square.x;
-        var d=square.delay;
-                //Интервал падения
-                var timerId = setInterval(function () 
-                {
-                   
-                   //очистка квадрата
-                   ctx.fillStyle = "#fff";
-                   ctx.fillRect(n,i-50,50,50)
-
-                    //отрисовка квадрата
-                    ctx.fillStyle = square.color;
-                    ctx.fillRect(n,i,square.height, square.width);
-                    
-                    
-
-                    i+=square.offsetY;
-
-                }
-                    ,square.delay);
-                //Интервал падения
-        
-                square.x = getRandX();
-                square.delay =getRandDelay();}
-                
-        , 1000);
-}
-
-
-
-
-
-
-function dropSquare(n){
-    
-    var i=square.y;
-        var timerId = setInterval(function () 
-        {
-            n//отрисовка квадрата
-            ctx.fillStyle = square.color;
-            ctx.fillRect(n,i,square.height, square.width);
-            
-            //очистка квадрата
-            ctx.fillStyle = "#fff";
-            ctx.fillRect(n,i-50,50,50)
-
-            i+=square.offsetY;
-
-        }
-            ,square.delay);
-
-    // alert(n);
-     
-
- }
-
-
- */
-
- /*function dropSquare(){
-    var i=square.y;
-    var n=square.x;
- 
-    for(i;i<650;i+=square.offsetY){
-     ctx.clearRect(n-50,Math.abs(i)-50,n, i);
-     ctx.fillRect(n,i,square.height, square.width);
-     
-     i+=square.offsetY;
-   
- }
- }
-*/
